86a5165a8f9ab3fe4df41773a8d19048d806755d
G2A\slawomir.lenczner <slenczner@g2a.com>
2018-08-29 07:22:00 +0200

    no message

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const coockieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const routes = require('./routes/index');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(coockieParser());
+
+app.use(flash());
+
+app.use('/', routes);
+
+module.exports = app;
